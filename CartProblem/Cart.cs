9bc5d61ac1b8682cc23aa056efc488a4971b6c28
Asmita Sharma <assharma@affinionds.com>
2019-08-08 01:06:44 +0530

    Cart Functionalities

---
+++ CartProblem/Cart.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace CartProblem
+{
+    public class Cart
+    {
+        private List<Product> _itemList;
+        private Dictionary<Product, int> _cartItemList;
+        private double _amount;
+
+
+        public Cart(List<Product> itemList)
+        {
+            _cartItemList = new Dictionary<Product, int>();
+            _itemList = itemList;           
+            _amount = 0;
+        }
+
+        public void AddItemToCart(Product product, int quantity)
+        {
+            if (_cartItemList.ContainsKey(product))
+            {
+                int updatedquantity = _cartItemList[product] + quantity;
+                _cartItemList[product] = updatedquantity;
+            }
+
+            else
+                _cartItemList.Add(product, quantity);            
+
+            // Calculating total amount after every change
+            CalculateTotalAmount();
+        }
+
+        public void RemoveItemFromCart(Product product, int quantity)
+        {
+          
+            if (_cartItemList[product] - quantity == 0)
+                _cartItemList.Remove(product);
+
+            
+            else
+                _cartItemList[product] = _cartItemList[product] - quantity;
+
+            
+            // Calculating total amount after every change
+            CalculateTotalAmount();
+        }
+
+        // Calculate total amount
+        public void CalculateTotalAmount()
+        {
+            foreach(var product in _cartItemList)
+            {
+                if (_itemList.Contains(product.Key))
+                {
+                    _amount += product.Value * product.Key.Price;
+                }
+            }         
+
+        }
+
+        public double GetAmount()
+        {
+            return _amount;
+        }
+    }
+}
---
+++ CartProblem/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace CartProblem
+{
+    public class Product
+    {
+        public string Name { get; private set; }
+
+        public double Price { get; private set; }
+
+        public string Category { get; private set; }
+
+        public void AddProduct(string productName, string productCategory, double productPrice)
+        {
+            Name = productName;
+            Category = productCategory;
+            Price = productPrice;
+        }
+    }
+}
